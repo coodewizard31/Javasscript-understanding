eda05b38ecea387ca8dbd64204955f02bd070d77
Frank <frankwamuyu40@gmail.com>
2026-06-17 15:45:56 +0300

    primitive data types

---
+++ JS/Intro/Primitive-Data-Type.js
@@ -0,0 +1,41 @@
+// Numbers
+// floating point number<decimal place
+let number1=239 //number
+console.log(number1)
+let number2 =34.34; //point no
+console.log(number2)
+let number3 =-344 //negative no
+console.log(number3)
+
+
+// strings to -> store a sequence of characters
+// 3 ways if creating strings
+// "Double quotes"
+// `use of backticks`
+// use of single quote string
+
+
+let str1="Double string i am Frank"
+console.log(str1)
+let str2="Sklningle string \n am Frank"
+console.log(str2)
+let str3="45"
+console.log(str3)
+
+
+// special -> \t >tab
+// newline ->\n
+
+// Booleans represents true of false statements
+let johnIsMale=true
+console.log(johnIsMale)
+let johnIsMarried=true
+console.log(johnIsMarried)
+// undefined -> shows lack of a value ie absense of a value
+// what iss the difference btwn undefined aand null
+// -> never explicity set something to undefined
+// if you have a variable does not have a value never use undefined use null
+let undefinedVal=undefined //DONTwrite this way undefined
+console.log(undefinedVal)
+let undefinedValue=null // do this instead of using undefined above.
+console.log(undefinedValue)
