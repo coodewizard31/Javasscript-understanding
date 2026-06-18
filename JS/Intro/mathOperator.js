45f5df4a35c6be5dde67df4a4a6378dc415ef738
Frank <frankwamuyu40@gmail.com>
2026-06-18 15:59:30 +0300

    operators & operands

---
+++ JS/Intro/mathOperator.js
@@ -0,0 +1,26 @@
+/* Assignment = */
+let numb1=23;//Declared and assigned the variable
+console.log(`num1 is${numb1} its type is ${typeof numb1}`)
+ num1=60;
+console.log(`num1 is ${num1} its type is ${typeof num1}`)
+let num2= 76
+console.log(`num2 is${num2}its type is ${typeof num2}`)
+let num3 = num1+num2+10
+console.log(`num3 is ${num3} its type is ${typeof num3}`)
+let num4 = num1*num2
+console.log(`num4 is ${num4} its type is ${typeof num4}`)
+let num5= num1**3
+console.log(`num5 is ${num5} its type is ${typeof num5}`)
+let num6=Math.pow(10,3)
+console.log(`num6 is ${num6} its type is ${typeof num6}`)
+
+//Division operator
+let numb7=30
+let numb8=10
+divNumb =num7/numb8
+console.log(`divNumb is ${divNumb} its type is ${typeof divNumb}`)
+//using mondulus operator
+let num9=23
+let number10=12
+let modNu=num9%number10
+console.log(`modNu is ${modNu} its type is ${typeof modNu}`)
