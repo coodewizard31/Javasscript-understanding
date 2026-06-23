69edb7827d0af3999e75e3a89f4989091763939e
Frank <frankwamuyu40@gmail.com>
2026-06-23 11:09:41 +0300

    short circuit

--- JS/Intro/andOrNot.js
+++ JS/Intro/andOrNot.js
@@ -21,3 +21,27 @@ let queue1="boy"
 let queue2="girl"
 let allowed=Boolean(queue1)===true && Boolean(queue2)===true
 console.log(`if not boy or girl gender not allowed!${allowed}`)
+console.log(`mercy is age is greater than 18 or she has more than 20k in her account=${mercy_age>18||mercy_balance<20000}`)
+
+/* Or atleast one statement has to be true for the whole statement to be true */
+console.log(`This is not true = ${!true}`)
+console.log(`This is not false =${!false}`)
+/* Truthy of falsey
+when you know something is not falsey then it is truthy
+falsey
+1.falsey
+2/0
+3.0
+4.null
+5.undefined
+6.''->empty string
+7 Nan -> not a number
+Anything  that is not falsey is truthy
+truthy||falsey ->truthy*/
+
+let userName1="John Mwangi" //truthy
+let userName2=0//falsey
+
+let activateUserName=UserName2 ||userName1
+//activateUserName->
+console.log(activeUserName)
