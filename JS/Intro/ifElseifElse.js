7e9899571ed2b362ef33d3ee9e2c05e05ffa9133
Frank <frankwamuyu40@gmail.com>
2026-06-24 11:32:16 +0300

    ifElse conditional statements and their workings

---
+++ JS/Intro/ifElseifElse.js
@@ -0,0 +1,27 @@
+/* lets start with if 
+it special it can be used alone */
+let age=prompt("Enter your age:")
+alert(`You entered${age}`)
+
+if(age>10){
+    alert("you are a baby")
+}
+//35>13=true
+if(age>10){
+    alert(`You are a baby.`)
+}
+if(age>13){
+    alert(`You are a teen`)
+}
+if(age>18){
+    alert(`You can legally smoke & drink`)
+}
+if(age>27){
+    alert(`Best age for marriage`)
+}
+if(age>45){
+    alert(`Best age for retirement`)
+}
+if(age>50){
+    alert(`You are an ansestor`)
+}
---
+++ JS/Intro/truthyAndFalsey.js
@@ -0,0 +1,40 @@
+/* scenario 1:
+1.declare a variable of your choice> assign a truthy value create an if statement and put the variable
+inside the if statement condition(variable).in side the if console.log("Scenario 1 it run") */
+let meatWeight=20
+if(meatWeight){
+    console.log(meatWeight)
+}
+
+/* Scenario 2:
+1.declare a variable< your choice>asssign a falsey value creat an if statement and put the variable inside the if statement.in side the if console.log("Scenario 2 it run") */
+let goatBreed
+if(goatBreed){
+    console.log(goatBreed)
+}
+/* Scenario 3:
+1.Create an if statement inside the if statement condition part. have true
+inside the block({}) console.log("scenario 3 it run")*/
+if(true){
+    console.log(true)
+
+}
+/* Scenario 4:
+1.create an if statement inside the if statement condition part.have false
+inside the block({}) console.log(scenario 4 it run)*/
+if(false){
+    console.log(false)
+}
+/* Scenario 5:
+1. create an if statement inside the if statement condition part.10>20
+inside the block({}) console.log("scenario 5 it run")*/
+if(10>20){
+    console.log(10>20)
+}
+/* Scenario 6:
+1.create an if statement inside the if statement conndition part. have 20>10
+inside the bock({}) console.log("scenario 6 it run")*/
+if(20>10){
+
+    console.log(20>10)
+}
