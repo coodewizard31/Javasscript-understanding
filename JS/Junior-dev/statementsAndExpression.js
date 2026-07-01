c9cd90ee18b15ade1f97fca4ea7d2d63b37d8928
Frank <frankwamuyu40@gmail.com>
2026-07-01 10:51:21 +0300

    Named & Anonymous functions

---
+++ JS/Junior-dev/statementsAndExpression.js
@@ -0,0 +1,21 @@
+/* Any piece of code that produces a value is an expression
+x>5 => true or false
+10+11=>21value
+circleArea(5)=>expression function call */
+
+let n1 = 23;
+function sayMyName() {
+  console.log("Sam Mwangi");
+}
+console.log(n1); //23 n1=23
+console.log(10 + 12); //10+12 //22
+console.log(n1 > 10); //n>10 or true or false
+console.log(sayMyName()); // function call=>undefined
+
+
+//STATEMENTS
+/* statements dictate action that should be done this include blocks */
+let n2=34// create stoe unit memory
+//block code->{} //statement if function ,loops
+console
+
---
+++ JS/Junior-dev/typesOfFunction.js
@@ -0,0 +1,36 @@
+/* 1.Name function
+2. Anonymous Function:
+a function can be assigned to be a variable
+a variable can reference a function*/
+function areaCircle(r) {
+  return 3.142 * r * r; // area of a function
+} //named function
+let age = 23;
+let samsAge = age; //age=23
+let ac = areaCircle;
+/* what is the value of ac and what is it
+->hint <use typeof>*/
+console.log(ac);
+/* if you want to create an anonymoous function
+1. create a function of your choice by using a named variable
+2.Assign that function a variable.
+3.Directly equate your variable to the function defination
+4.remove the original function {still works by calling the variable but calling the function fails}
+5.try calling the variable & the original function
+6.Remove the name of a function,since its now rendered useless
+7.you remain with the called anonymous function*/
+//Function for the area of a rectangle
+function rectangleArea(l, w) {
+  return l * w;
+}
+let rA = function rectangleArea(l, w) {
+  return l * w;
+};
+
+
+function belief(){
+    console.log(`Father+Son+HolySpirit`)
+}
+let BV= function belief(){
+    console.log(`Father+Son+HolySpirit`)
+}
