e01662a9d793d0038346d7449ea58803abb9165b
Frank <frankwamuyu40@gmail.com>
2026-07-07 15:01:26 +0300

    for loops application

--- JS/Junior-dev/forLoop.js
+++ JS/Junior-dev/forLoop.js
@@ -24,3 +24,50 @@ while (k < 20) {
 for (let k = 0; k < 20; k = k + 1) {
   console.log("k", k);
 }
+
+
+
+function promptStudent() {
+  let num1 = null;
+  let num2 = null;
+  
+  while (true) {
+    num1 = prompt("Enter the first number");
+    if (isNaN(num1)) continue; 
+
+    num2 = prompt("Enter the second number");
+    if (isNaN(num2)) continue; 
+
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    
+    if (num1 <= 0 || num2 <= 0) continue; 
+    
+    break;
+  }
+  
+  console.log("Got valid numbers", num1, num2);
+  mathTable(num1, num2);
+}
+
+function mathTable(num1, num2) {
+  if (!num1 || num1 < 0 || !num2 || num2 < 0) {
+    console.error("Ensure number1 or number2 are numbers greater than 0");
+    return;
+  }
+
+  // REFACTOR: Replaced while loop with a decreasing for loop
+  for (let outerLoop = num1; outerLoop > 0; outerLoop--) {
+    doInnerLoop(outerLoop, num2); 
+  }
+}
+
+// individual // test separate
+function doInnerLoop(outerLoop, num2) {
+  // REFACTOR: Replaced while loop with a decreasing for loop
+  for (let innerLoop = num2; innerLoop > 0; innerLoop--) {
+    console.log(`outerLoop=${outerLoop} innerLoop=${innerLoop}`);
+    console.log(`${outerLoop}*${innerLoop}=${outerLoop * innerLoop}`);
+  }
+}
+mathTable(7,8)
