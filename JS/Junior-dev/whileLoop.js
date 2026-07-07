0aa98dd82181d819da0ff52028be7df4d60fb469
Frank <frankwamuyu40@gmail.com>
2026-07-07 10:13:19 +0300

    while loop

--- JS/Junior-dev/whileLoop.js
+++ JS/Junior-dev/whileLoop.js
@@ -10,3 +10,68 @@ while (condition) {
   } //statement 1
 }
 //the condition remains true unless a false condition is meet & it breaks out of the code
+
+function vault(n) {
+  //looping
+  while (n > 0) {
+    console.log(n);
+    n = n - 1;
+  }
+}
+vault(7);
+
+function promptStudent() {
+  let num1 = null;
+  let num2 = null;
+  while (true) {
+    num1 = prompt("Enter the first number");
+    if (isNaN(num1)) {
+      continue; //restart the loop
+    }
+    num2 = prompt("Enter the second number");
+    if (isNaN(num2)) {
+      continue; //restart the loop from begining
+    }
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (num1 <= 0 || num2 <= 0) {
+      continue; //
+    }
+    break;
+  }
+  console.log("Got valid number", num1, num2);
+  mathTable(num1, num2);
+}
+
+function mathTable(num1, num2) {
+  if (!num1 || num1 < 0 || !num2 || num2 < 0) {
+    console.error("Ensure number1 or number2 are numbers greater than 0");
+    return;
+  }
+
+  let outerLoop = num1; //2
+
+  while (outerLoop > 0) {
+    doInnerLoop(outerLoop, num2); //
+    outerLoop = outerLoop - 1;
+  }
+  // while (outerLoop > 0) {
+  //   let innerLoop = num2; //3 //3
+  //   while (innerLoop > 0) {
+  //      console.log(`outerLoop=${outerLoop} innerLoop=${innerLoop}`);
+  //      console.log(`${outerLoop}*${innerLoop}=${outerLoop * innerLoop}`);
+  //      innerLoop = innerLoop - 1;
+  //   }
+  //   outerLoop = outerLoop - 1;
+  // }
+}
+
+//individual//test separate
+function doInnerLoop(outerLoop, num2) {
+  let innerLoop = num2;
+  while (innerLoop > 0) {
+    console.log(`outerLoop=${outerLoop} innerLoop=${innerLoop}`);
+    console.log(`${outerLoop}*${innerLoop}=${outerLoop * innerLoop}`);
+    innerLoop = innerLoop - 1;
+  }
+}
